9422a61ff5b10d546497eae1e735606ae91ddbc2
perupoguvenkatesh <perupoguvenkatesh5073@gmail.com>
2026-03-04 16:57:03 +0530

    written extra code for Sum component

--- src/Components/Sum.js
+++ src/Components/Sum.js
@@ -1,8 +1,10 @@
 function Sum(props){
     const {a,b}=props
+    const c=a+b;
     return(
         <div>
-            <h1>Rahul</h1>
+            <h1>{c}</h1>
+            <h1>It is the Sum of {a} and {b} is {c}</h1>
         </div>
     )
 }
