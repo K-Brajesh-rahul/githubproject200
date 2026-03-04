4da353cd4ec57e47225e4160bf35b61c6c934446
beastpluse <brajeshrahul123@gmail.com>
2026-03-04 16:43:08 +0530

    Adding Sum Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Rahul from "./Components/Rahul"
 import Venkatesh from "./Components/Venkatesh"
 import Venkatesh from "./Components/Charan"
+import Sum from "./Components/Sum"
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
       <Rahul/>
       <Venkatesh/>
       <Charan/>
+      <Sum a={91} b={44}/>
     </div>
   );
 }
---
+++ src/Components/Sum.js
@@ -0,0 +1,18 @@
+// function Sum(props){
+//     const {a,b}=props
+//     return(
+//         <div>
+//             <h1>Rahul</h1>
+//         </div>
+//     )
+// }
+// export default Sum
+
+function Sum(){
+    return(
+        <div>
+            <h1>Sum</h1>
+        </div>
+    )
+}
+export default Sum;
