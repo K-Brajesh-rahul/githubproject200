80f33a2e045f860df60b8383e99975ae6233278a
perupoguvenkatesh <perupoguvenkatesh5073@gmail.com>
2026-03-04 15:59:07 +0530

    charan component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Rahul from "./Components/Rahul"
 import Venkatesh from "./Components/Venkatesh"
+import Venkatesh from "./Components/Charan"
 function App() {
   return (
     <div className="App">
       <h1>Github Project 200</h1>
       <Rahul/>
       <Venkatesh/>
+      <Charan/>
     </div>
   );
 }
---
+++ src/Components/Charan.js
@@ -0,0 +1,8 @@
+function Charan(){
+    return(
+        <div>
+            <h1>Charan</h1>
+        </div>
+    )
+}
+export default Charan
