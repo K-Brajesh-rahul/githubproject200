d90b3e472de35e8461d49077fb61b48212054736
beastpluse <brajeshrahul123@gmail.com>
2026-03-04 16:28:30 +0530

    changing charan heading

--- src/Components/Charan.js
+++ src/Components/Charan.js
@@ -1,9 +1,9 @@
 function Charan(){
     return(
         <div>
-            <h1>Charan sir is added</h1>
-            <h1>Charan at middle</h1>
-            <h1>Charan sir at bottom</h1>
+            <h1>Charan is added</h1>
+            <h1>Charan is at middle</h1>
+            <h1>Charan is at end</h1>
         </div>
     )
 }
