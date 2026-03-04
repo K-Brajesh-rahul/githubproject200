33c0b47e1593cad17097d4fbff339d6e72a0c0ad
beastpluse <brajeshrahul123@gmail.com>
2026-03-04 16:29:29 +0530

    removing heading of charan

--- src/Components/Charan.js
+++ src/Components/Charan.js
@@ -2,8 +2,6 @@ function Charan(){
     return(
         <div>
             <h1>Charan is added</h1>
-            <h1>Charan is at middle</h1>
-            <h1>Charan is at end</h1>
         </div>
     )
 }
